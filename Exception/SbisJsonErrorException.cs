c29ab8629462131d712293e86265477e89aa4f88
Alexandr Nekrasov <nekalex@gmail.com>
2014-04-14 17:46:20 +0600

    release

---
+++ Exception/SbisJsonErrorException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RU.NekAlex.Sbis.SDK;
+
+namespace RU.NekAlex.Sbis.Exception
+{
+        public class SbisJsonErrorException : ApplicationException
+        {
+            private string response { get; set; }
+            public SbisJsonErrorException(string response)
+            {
+                this.response = response;
+            }
+            public override string ToString()
+            {
+                return SBisRequests.GetErrorInfo(this.response).ToString();
+                //return base.ToString();
+            }
+        }
+}
---
+++ SDK/SbisSDK.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RU.NekAlex.Sbis.SDK
+{
+    [System.Xml.Serialization.XmlRoot("Конверт")]
+    public class SBiSKonvert
+    {
+        public string ТипДокумента { get; set; }
+        public SBiSKonvertWho Отправитель { get; set; }
+        public SBiSKonvertWho Получатель { get; set; }
+        public string Дата { get; set; }
+        public string Номер { get; set; }
+        public string Примечание { get; set; }
+        [System.Xml.Serialization.XmlArray]
+        public List<SBiSKonverAtta> Вложения { get; set; }
+        public SBiSKonvert()
+        {
+            Отправитель = new SBiSKonvertWho();
+            Получатель = new SBiSKonvertWho();
+            Вложения = new List<SBiSKonverAtta>();
+        }
+    }
+    public class SBiSKonvertWho
+    {
+        [System.Xml.Serialization.XmlAttribute]
+        public string ИНН { get; set; }
+        [System.Xml.Serialization.XmlAttribute]
+        public string КПП { get; set; }
+        [System.Xml.Serialization.XmlAttribute]
+        public string Название { get; set; }
+    }
+    [System.Xml.Serialization.XmlType("Вложение")]
+    public class SBiSKonverAtta
+    {
+        public string ИдентификаторДокумента { get; set; }
+        public string ИмяФайла { get; set; }
+        public string Название { get; set; }
+        public string Версия { get; set; }
+        public string Выгружен { get; set; }
+        public string Служебный { get; set; }
+        public string ЧислоПодписей { get; set; }
+        public string ТипДокумента { get; set; }
+        public string Человечный { get; set; }
+    }
+}
